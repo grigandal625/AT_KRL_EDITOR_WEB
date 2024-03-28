5610e911c113599a4ee66eefc6fde0349fa8fa1a
Andrew <grigandal625@gmail.com>
2024-03-29 00:38:03 +0300

    fix reseting value type

--- frontend/src/components/knowledge_base/objects/events/EventOccuranceConditionForm.jsx
+++ frontend/src/components/knowledge_base/objects/events/EventOccuranceConditionForm.jsx
@@ -4,7 +4,14 @@ import SimpleFormulaEditor from "../../../../utils/formula_editor/SimpleFormulaE
 export default ({ form, onValuesChange, ...props }) => {
     return (
         <Form form={form} onValuesChange={onValuesChange} layout="vertical" {...props}>
-            <Form.Item name="occurance_condition" label="Условие возникновения">
+            <Form.Item
+                name="occurance_condition"
+                label="Условие возникновения"
+                rules={[
+                    { required: true, message: "Укажите условие возникновения" },
+                    { validator: (_, value) => (value?.tag ? Promise.resolve() : Promise.reject(new Error("Укажите условие возникновения"))) },
+                ]}
+            >
                 <SimpleFormulaEditor />
             </Form.Item>
         </Form>
--- frontend/src/components/knowledge_base/objects/intervals/IntervalOpenCloseForm.jsx
+++ frontend/src/components/knowledge_base/objects/intervals/IntervalOpenCloseForm.jsx
@@ -4,10 +4,24 @@ import SimpleFormulaEditor from "../../../../utils/formula_editor/SimpleFormulaE
 export default ({ form, onValuesChange, ...props }) => {
     return (
         <Form onValuesChange={onValuesChange} form={form} {...props}>
-            <Form.Item name="open" label="Условие начала">
+            <Form.Item
+                name="open"
+                label="Условие начала"
+                rules={[
+                    { required: true, message: "Укажите условие начала" },
+                    { validator: (_, value) => (value?.tag ? Promise.resolve() : Promise.reject(new Error("Укажите условие начала"))) },
+                ]}
+            >
                 <SimpleFormulaEditor />
             </Form.Item>
-            <Form.Item name="close" label="Условие окончания">
+            <Form.Item
+                name="close"
+                label="Условие окончания"
+                rules={[
+                    { required: true, message: "Укажите условие окончания" },
+                    { validator: (_, value) => (value?.tag ? Promise.resolve() : Promise.reject(new Error("Укажите условие окончания"))) },
+                ]}
+            >
                 <SimpleFormulaEditor />
             </Form.Item>
         </Form>
